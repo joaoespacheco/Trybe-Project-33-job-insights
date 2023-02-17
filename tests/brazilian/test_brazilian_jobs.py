02efa1bb72427086de14251e6c7623f9219f45cb
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2023-02-17 14:41:30 -0300

    test: Implementa test_brasilizian_jobs para a funcionalidade read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+PATH = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    responses = read_brazilian_file(PATH)
+    for response in responses:
+        assert "title" in response
+        assert "salary" in response
+        assert "type" in response
