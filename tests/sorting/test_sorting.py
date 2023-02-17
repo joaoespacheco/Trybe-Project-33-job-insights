d5e0d16f6576a1ad9fceadbf06dde7ec8d10163e
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2023-02-17 16:46:49 -0300

    fix: Alteração de mock para o teste

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -4,13 +4,40 @@ from src.insights.jobs import read
 
 PATH = "tests/mocks/sorting.csv"
 
+DATA = [
+    {
+        "title": "Web developer",
+        "min_salary": 1000,
+        "max_salary": 3000,
+        "date_posted": "2020-06-07",
+    },
+    {
+        "title": "Front end developer",
+        "min_salary": 2000,
+        "max_salary": 2000,
+        "date_posted": "2021-06-01",
+    },
+    {
+        "title": "Back end developer",
+        "min_salary": 3000,
+        "max_salary": 3000,
+        "date_posted": "2020-10-09",
+    },
+    {
+        "title": "Full stack end developer",
+        "min_salary": 4000,
+        "max_salary": 8000,
+        "date_posted": "2021-01-01",
+    },
+]
+
 
 def test_sort_by_criteria():
-    sorted_min = read(PATH)
+    sorted_min = DATA
     sort_by(sorted_min, "min_salary")
-    sorted_max = read(PATH)
+    sorted_max = DATA
     sort_by(sorted_max, "max_salary")
-    sorted_date = read(PATH)
+    sorted_date = DATA
     sort_by(sorted_date, "date_posted")
     first_date = datetime.strptime(sorted_date[0]["date_posted"], "%Y-%m-%d")
     last_date = datetime.strptime(
